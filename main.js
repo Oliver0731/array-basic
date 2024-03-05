ba912eebda777258290b5333c379bb98ba4bcd58
Oliver0731 <112982163+Oliver0731@users.noreply.github.com>
2024-03-05 09:37:41 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+let vowels = ["a", "e", "i", "o", "u", "y"];
+
+
+console.log(vowels[1]);
+console.log(vowels[2]);
+console.log(vowels[5]);
+console.log(vowels[6]);
+console.log(vowels.length);
+
+//2.
+let nums = ["Odd #'s", 1, 7, 11, 5];
+
+
+nums[0] = "Even #'s";
+nums[1]++;
+nums[2] += 5;
+nums[3]--;
+nums[4] *= 2;
+console.log(nums);
+//3. 
+
+let msg = ["Caleb", "and", "Clara", "ate", 2, "yellow", "bananas", "today"];
+
+
+msg.pop();
+msg.push("yesterday");
+msg[4] += 18;
+msg[5] = "brown";
+msg.splice(2, 1);
+msg.splice(1, 1);
+console.log(msg);
+
+//4.0
