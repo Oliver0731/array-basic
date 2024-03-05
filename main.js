f1a2a4807492dd76f0700c7131d35374a81985d0
Oliver0731 <o.townell@share.epsb.ca>
2024-03-05 09:59:56 -0700

    .update

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
-let vowels = ["a", "e", "i", "o", "u", "y"];
+// let vowels = ["a", "e", "i", "o", "u", "y"];
 
+// console.log(vowels[1]);
+// console.log(vowels[2]);
+// console.log(vowels[5]);
+// console.log(vowels[6]);
+// console.log(vowels.length);
 
-console.log(vowels[1]);
-console.log(vowels[2]);
-console.log(vowels[5]);
-console.log(vowels[6]);
-console.log(vowels.length);
+// //2.
+// let nums = ["Odd #'s", 1, 7, 11, 5];
 
-//2.
-let nums = ["Odd #'s", 1, 7, 11, 5];
+// nums[0] = "Even #'s";
+// nums[1]++;
+// nums[2] += 5;
+// nums[3]--;
+// nums[4] *= 2;
+// console.log(nums);
+// //3.
 
+// let msg = ["Caleb", "and", "Clara", "ate", 2, "yellow", "bananas", "today"];
 
-nums[0] = "Even #'s";
-nums[1]++;
-nums[2] += 5;
-nums[3]--;
-nums[4] *= 2;
-console.log(nums);
-//3. 
+// msg.pop();
+// msg.push("yesterday");
+// msg[4] += 18;
+// msg[5] = "brown";
+// msg.splice(2, 1);
+// msg.splice(1, 1);
+// console.log(msg);
 
-let msg = ["Caleb", "and", "Clara", "ate", 2, "yellow", "bananas", "today"];
+// //4.0
 
-
-msg.pop();
-msg.push("yesterday");
-msg[4] += 18;
-msg[5] = "brown";
-msg.splice(2, 1);
-msg.splice(1, 1);
-console.log(msg);
-
-//4.0
+function arrayInfo(anArray) {
+  anArray[3] += 2;
+  anArray[4] *= 0.5;
+  anArray.splice(1, 2);
+  console.log(anArray);
+}
+arrayInfo([2, 4, 6, 8, 10]);
